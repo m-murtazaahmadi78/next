90f31fe43c9221312951ff7ba79897a8a8d758ce
Murtaza Ahmadi <m.murtaza.ahmadi78@gmail.com>
2025-09-21 18:25:55 +0430

    Stop tracking all files

--- app/ui/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Inter, Lusitana } from 'next/font/google';
-export const inter = Inter({ subsets: ['latin'] });
-export const Lusitanas=Lusitana({
-    weight:['400','700'],
-    subsets:['latin'],
-})
