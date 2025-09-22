39658d715a5345aa10afa34c5bfe1258d964afb7
Murtaza Ahmadi <m.murtaza.ahmadi78@gmail.com>
2025-09-22 11:57:27 +0430

    the error has been solved

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,11 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { Lusitanas } from '@/app/ui/fonts';
+import {lusitana} from '@/app/ui/fonts'
+
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${Lusitanas.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Shopping</p>
